34354b0a9ad695cd863f66f0e709bf937a3c1c05
Ricardo Reis <ricardo@significa.co>
2023-02-19 19:28:24 +0000

    feat(ui): icons

---
+++ src/ui/icon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts" context="module">
+	import home from './icons/home.svg?raw';
+	import discover from './icons/discover.svg?raw';
+	import movie from './icons/movie.svg?raw';
+	import music from './icons/music.svg?raw';
+	import photo from './icons/photo.svg?raw';
+	import tv from './icons/tv.svg?raw';
+	import tvshow from './icons/tvshow.svg?raw';
+
+	export const icons = {
+		home,
+		discover,
+		movie,
+		music,
+		photo,
+		tv,
+		tvshow
+	};
+
+	export type IconOptions = keyof typeof icons;
+
+	export const isIconVariant = (variant: string | undefined): variant is IconOptions => {
+		if (!variant) return false;
+		return Object.keys(icons).includes(variant);
+	};
+</script>
+
+<script lang="ts">
+	let className: string | undefined = undefined;
+	export { className as class };
+
+	export let icon: IconOptions;
+</script>
+
+<i data-icon={icon} class={className} aria-hidden="true" {...$$restProps}>
+	{@html icons[icon]}
+</i>
