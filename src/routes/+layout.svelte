7d2aa737a317c7aa3f042d511682d984653dca51
Ricardo Reis <ricardo@significa.co>
2023-02-19 19:20:58 +0000

    feat(ui): background

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<div class="bg">
+	<div />
+</div>
+
+<style>
+	.bg {
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+
+		background-color: #3f4245;
+	}
+
+	.bg > div {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: radial-gradient(
+				circle farthest-side at 0% 100%,
+				rgb(47, 47, 47) 0%,
+				rgba(47, 47, 47, 0) 100%
+			),
+			radial-gradient(
+				circle farthest-side at 100% 100%,
+				rgb(63, 63, 63) 0%,
+				rgba(63, 63, 63, 0) 100%
+			),
+			radial-gradient(circle farthest-side at 100% 0%, rgb(76, 76, 76) 0%, rgba(76, 76, 76, 0) 100%),
+			radial-gradient(circle farthest-side at 0% 0%, rgb(58, 58, 58) 0%, rgba(58, 58, 58, 0) 100%),
+			black;
+	}
+</style>
