686a490ffbda129d4768db8c0f7cc50f07f818cd
Ricardo Reis <ricardo@significa.co>
2023-02-21 01:00:27 +0000

    feat(media-card): badge

--- src/components/ui/media-card.svelte
+++ src/components/ui/media-card.svelte
@@ -1,101 +1,131 @@
 <script lang="ts">
-	let className = '';
-	export { className as class };
-	export let title: string;
-	export let subtitle: string;
-	export let href: string;
-	export let description: string | undefined = undefined;
+  let className = '';
+  export { className as class };
+  export let title: string;
+  export let subtitle: string;
+  export let href: string;
+  export let description: string | undefined = undefined;
+  export let badge: string | number | undefined = undefined;
 </script>
 
 <article class="media-card {className}">
-	<div class="card">
-		<a {href} {title}>
-			<img src="https://i.imgur.com/Slm02LL.png" />
-		</a>
-	</div>
-	<div class="info">
-		<a class="title" {href} {title}>
-			{title}
-		</a>
-		<p class="subtitle">
-			{subtitle}
-		</p>
-		{#if description}
-			<p class="description">
-				{description}
-			</p>
-		{/if}
-	</div>
+  <div class="card">
+    <a {href} {title}>
+      <img src="https://i.imgur.com/Slm02LL.png" />
+    </a>
+    {#if badge}
+      <div class="badge">
+        {badge}
+      </div>
+    {/if}
+  </div>
+  <div class="info">
+    <a class="title" {href} {title}>
+      {title}
+    </a>
+    <p class="subtitle">
+      {subtitle}
+    </p>
+    {#if description}
+      <p class="description">
+        {description}
+      </p>
+    {/if}
+  </div>
 </article>
 
 <style lang="postcss">
-	.card {
-		margin-bottom: 8px;
-		background-color: rgba(0, 0, 0, 0.45);
-		border-radius: 4px;
-		box-shadow: 0 0 4px rgb(0 0 0 / 30%);
-		position: relative;
+  .card {
+    margin-bottom: 8px;
+    background-color: rgba(0, 0, 0, 0.45);
+    border-radius: 4px;
+    box-shadow: 0 0 4px rgb(0 0 0 / 30%);
+    position: relative;
 
-		overflow: hidden;
-		aspect-ratio: 1 / 1.5;
-		max-width: 200px;
+    overflow: hidden;
+    aspect-ratio: 1 / 1.5;
+    max-width: 200px;
 
-		& img {
-			width: 100%;
-			height: 100%;
-			object-fit: cover;
-		}
+    & a {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
 
-		@media --hover {
-			&:hover {
-				border-radius: 4px;
-				box-shadow: 0 0 0 1px #e5a00d, 0 0 4px rgb(0 0 0 / 30%);
+    & img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 
-				& + .info .title {
-					text-decoration: underline;
-				}
+    & .badge {
+      @mixin text-semi;
 
-				&::before {
-					content: '';
-					position: absolute;
-					top: 0;
-					left: 0;
-					width: 100%;
-					height: 100%;
-					background-color: rgba(30, 30, 30, 0.6);
-					pointer-events: none;
-					user-select: none;
-				}
-			}
-		}
-	}
+      pointer-events: none;
+      position: absolute;
+      top: 4px;
+      right: 4px;
 
-	.info {
-		padding-inline: 4px;
-		cursor: default;
+      border-radius: 4px;
+      line-height: 22px;
+      min-width: 28px;
+      padding: 0 6px;
+      text-align: center;
 
-		& > * {
-			display: block;
+      background-color: #e5a00d;
+      color: #1f2326;
+    }
 
-			line-height: 24px;
-			text-overflow: ellipsis;
-			white-space: nowrap;
+    @media --hover {
+      &:hover {
+        border-radius: 4px;
+        box-shadow: 0 0 0 1px #e5a00d, 0 0 4px rgb(0 0 0 / 30%);
 
-			overflow: hidden;
-		}
-	}
+        & + .info .title {
+          text-decoration: underline;
+        }
 
-	.title {
-		color: #fff;
+        &::before {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(30, 30, 30, 0.6);
+          pointer-events: none;
+          user-select: none;
+        }
+      }
+    }
+  }
 
-		@media --hover {
-			&:hover {
-				text-decoration: underline;
-			}
-		}
-	}
+  .info {
+    padding-inline: 4px;
+    cursor: default;
 
-	.subtitle {
-		color: hsla(0, 0%, 100%, 0.45);
-	}
+    & > * {
+      display: block;
+
+      line-height: 24px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+
+      overflow: hidden;
+    }
+  }
+
+  .title {
+    color: #fff;
+
+    @media --hover {
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .subtitle {
+    color: hsla(0, 0%, 100%, 0.45);
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-	import { page } from '$app/stores';
-	import MediaCard from '$components/ui/media-card.svelte';
+  import { page } from '$app/stores';
+  import MediaCard from '$components/ui/media-card.svelte';
 </script>
 
-<MediaCard title="007 - Alvo em Movimento" subtitle="1985" href="/" />
+<MediaCard title="007 - Alvo em Movimento" subtitle="1985" badge="39" href="/007" />
 
 <pre>
 {JSON.stringify($page.data.data, null, 2)}
 </pre>
 
 <style>
-	pre {
-		max-width: 100%;
-		white-space: pre-wrap;
-	}
+  pre {
+    max-width: 100%;
+    white-space: pre-wrap;
+  }
 </style>
