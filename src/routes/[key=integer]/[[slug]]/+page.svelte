dcfa9d58b8b29b5d6c56e99328a76b6415dddea7
Ricardo Reis <ricardo@significa.co>
2023-03-04 23:18:24 +0000

    refactor: library layout to page

--- src/routes/[key=integer]/[[slug]]/+page.svelte
+++ src/routes/[key=integer]/[[slug]]/+page.svelte
@@ -1,17 +1,41 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import MediaCard from '$components/ui/media-card.svelte';
+  import { libraries } from '$lib/stores';
+
+  $: library = $libraries.find((lib) => lib.key === +$page.params.key);
 </script>
 
-{#each $page.data.media.data as media (media.ratingKey)}
-  <MediaCard
-    title={media.title}
-    subtitle={media.year}
-    badge="39"
-    href="{$page.url.pathname}/{media.ratingKey}"
-    image={{
-      src: `/img/thumb/${media.ratingKey}.png`,
-      alt: ''
-    }}
-  />
-{/each}
+<svelte:head>
+  {#if library}
+    <title>{library.title} • Plexlv</title>
+
+    <meta name="og:url" content={$page.url.toString()} />
+    <meta name="og:title" content={library.title} />
+  {/if}
+</svelte:head>
+
+<main>
+  {#if $page.data?.media?.data}
+    {#each $page.data.media.data as media (media.ratingKey)}
+      <MediaCard
+        title={media.title}
+        subtitle={media.year}
+        badge="39"
+        href="{$page.url.pathname}/{media.ratingKey}"
+        image={{
+          src: `/img/thumb/${media.ratingKey}.png`,
+          alt: ''
+        }}
+      />
+    {/each}
+  {/if}
+</main>
+
+<style lang="postcss">
+  main {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(130px, 1fr));
+    gap: 30px;
+  }
+</style>
