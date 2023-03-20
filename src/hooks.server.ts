20e29fa2ec060a1b8c63e597d646c54a45af770a
Ricardo Reis <ricardo@significa.co>
2023-03-20 16:18:04 +0000

    chore: hooks house cleaning

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,31 +1,5 @@
-import { isValidLocale } from './lib/data';
-import { LANG_COOKIE_KEY, LANG_SEARCH_PARAM_KEY } from '$lib/constants';
-import { getValidLocale } from '$lib/data';
-import { redirect, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import type { Handle } from '@sveltejs/kit';
+import { lang } from '$lib/hooks';
 
-export const handle = (async ({ event, resolve }) => {
-  const paramsLang = event.url.searchParams.get(LANG_SEARCH_PARAM_KEY);
-
-  if (paramsLang) {
-    if (isValidLocale(paramsLang)) {
-      event.cookies.set(LANG_COOKIE_KEY, paramsLang, {
-        path: '/'
-      });
-    }
-
-    event.url.searchParams.delete(LANG_SEARCH_PARAM_KEY);
-
-    const url = `${event.url.pathname}?${event.url.searchParams.toString()}`;
-
-    throw redirect(302, url);
-  }
-
-  const lang = getValidLocale([event.cookies.get(LANG_COOKIE_KEY)]);
-  event.locals.lang = lang;
-
-  const response = await resolve(event, {
-    transformPageChunk: ({ html }) => html.replace('%lang%', lang)
-  });
-
-  return response;
-}) satisfies Handle;
+export const handle = sequence(lang) satisfies Handle;
