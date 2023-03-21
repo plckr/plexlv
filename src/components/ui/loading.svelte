1b05b7c7266f60f93b1a69ddbf0f430764435313
Ricardo Reis <ricardo@significa.co>
2023-03-21 17:34:06 +0000

    feat(ui): loading

---
+++ src/components/ui/loading.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div class="loading" />
+
+<style lang="postcss">
+  @keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(1turn);
+    }
+  }
+
+  .loading {
+    display: inline-block;
+
+    height: 24px;
+    width: 24px;
+
+    border-style: solid;
+    border-color: #cc7b19 transparent transparent #cc7b19;
+    border-width: 2px;
+
+    border-radius: 50%;
+
+    animation: loading 0.6s linear infinite;
+  }
+</style>
