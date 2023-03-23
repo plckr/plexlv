958393bb06db2371c6f5f86a98f76c4f7c3d5f98
Ricardo Reis <ricardo@significa.co>
2023-03-23 10:27:19 +0000

    chore: library view naming

--- src/params/libraryView.ts
+++ src/params/libraryView.ts
@@ -3,10 +3,10 @@ import type { ParamMatcher } from '@sveltejs/kit';
 const types = ['all', 'collections'] as const;
 export type LibraryView = (typeof types)[number];
 
-export const isImgType = (param: string): param is LibraryView => {
+export const isLibraryView = (param: string): param is LibraryView => {
   return (types as ReadonlyArray<string>).includes(param);
 };
 
 export const match = ((param) => {
-  return isImgType(param);
+  return isLibraryView(param);
 }) satisfies ParamMatcher;
