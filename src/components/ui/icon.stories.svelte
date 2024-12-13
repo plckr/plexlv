823539fdb225fb98c147f9dc6da5617a81f4b526
Ricardo Reis <ricardo@plckr.dev>
2024-12-13 20:33:16 +0000

    fix stories errors

---
+++ src/components/ui/icon.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import { objectKeys } from '$lib/utils/object';
+  import Icon, { icons } from './icon.svelte';
+
+  const { Story } = defineMeta({
+    title: 'UI/Icon',
+    component: Icon,
+    tags: ['autodocs'],
+    args: { icon: 'home' },
+    argTypes: {
+      icon: {
+        control: { type: 'select' },
+        options: objectKeys(icons)
+      }
+    }
+  });
+</script>
+
+<Story name="Default" />
