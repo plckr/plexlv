dba755ed19c2b0768b6f6548ebb845f80fa60d10
Ricardo Reis <ricardo@plckr.dev>
2024-12-17 22:51:13 +0000

    storybook build workaround

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import type { StorybookConfig } from '@storybook/sveltekit';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import Icons from 'unplugin-icons/vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
@@ -16,7 +18,23 @@ const config: StorybookConfig = {
   },
   docs: {},
   async viteFinal(config) {
+    config = workaroundSvelteDocgenPluginConflictWithUnpluginIcons(config);
+
     return mergeConfig(config, {
+      plugins: [
+        Icons({
+          compiler: 'svelte',
+          iconCustomizer(collection, _icons, props) {
+            if (collection === 'plex') {
+              props.width = 'auto';
+              props.height = 'auto';
+            }
+          },
+          customCollections: {
+            plex: FileSystemIconLoader('./src/components/ui/icons')
+          }
+        })
+      ],
       resolve: {
         alias: {
           $app: path.resolve(process.cwd(), './__mocks__/$app'),
@@ -31,3 +49,28 @@ const config: StorybookConfig = {
   }
 };
 export default config;
+
+// https://github.com/storybookjs/storybook/issues/20562
+/**
+ * @param {import('vite').InlineConfig} config
+ */
+const workaroundSvelteDocgenPluginConflictWithUnpluginIcons = (config) => {
+  if (!config.plugins) return config;
+
+  const [_internalPlugins, ...userPlugins] = config.plugins;
+  const docgenPlugin = userPlugins.find(
+    (plugin) => plugin.name === 'storybook:svelte-docgen-plugin'
+  );
+  if (docgenPlugin) {
+    const origTransform = docgenPlugin.transform;
+    const newTransform = (code, id, options) => {
+      if (id.startsWith('~icons/')) {
+        return;
+      }
+      return origTransform?.call(docgenPlugin, code, id, options);
+    };
+    docgenPlugin.transform = newTransform;
+    docgenPlugin.enforce = 'post';
+  }
+  return config;
+};
